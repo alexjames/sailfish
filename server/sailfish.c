c101489658f36cf3907cd12e91f3aa619e90b853
alexjames <alx.james@gmail.com>
2015-03-08 03:32:44 +0530

    Code re-org. Added SO_USEREADDR socket option.

--- server/sailfish.c
+++ server/sailfish.c
@@ -6,39 +6,78 @@
 #include <netinet/in.h>
 #include "sailfish.h"
 
-void set_up_listener()
+int set_up_listener(int port)
 {
+    /* Initialize listening socket */
+    int optval = 1;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    sa.sin_addr.s_addr = INADDR_ANY;
+    memset(&sa.sin_zero, 0, 8);
 
+    // Set SO_REUSEADDR socket option, otherwise we'll be plagued with 'Address already in use'
+    // errors. Common wisdom says everything will be OK.
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1)
+    {
+        perror("setsocketopt");
+        return (-1);
+    }
+    if (bind(sockfd, (struct sockaddr *)&sa, sizeof(struct sockaddr)) == -1)
+    {
+        perror("bind");
+        return (-1);
+    }
+    if (listen(sockfd, 10) == -1)
+    {
+        perror("listen");
+        return (-1);
+    }
+    return sockfd;
 }
 
-void process_request()
+void process_request(int in_soc, struct sockaddr_in sa)
 {
+    char msg[] = "Thank you for using SailFish. Have a nice day. :)\n";
+    int sent = send(in_soc, msg, sizeof(msg), 0);
+    printf ("\nWrote %d bytes over connection", sent);
+    close(in_soc);
+}
 
-
+void listen_loop(int sockfd)
+{
+    struct sockaddr_in in_sa;
+    int conn, len;
+    while (1)
+    {
+        conn = accept(sockfd, (struct sockaddr *)&in_sa, &len);
+        process_request(conn, in_sa);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int port;
-    int sockfd;
+    int specified_port = atoi(argv[1]);
+    int primary_listener;     /* Primary listening socket FD */
+    int len;
 
     if (argc < 2)
     {
         printf("Invalid usage.\n");
         print_usage();
+        return 0;
     }
 
-    /* Initialize primary socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in sa;
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons(atoi(argv[1]));
-    sa.sin_addr.s_addr = INADDR_ANY;
-    memset(&sa.sin_zero, 0, 8);
+    primary_listener = set_up_listener(specified_port);
 
-    bind(sockfd, (struct sockaddr *)&sa, sizeof(struct sockaddr));
+    if (primary_listener < 0)
+    {
+        printf("Looks like we are having some problems. SailFish will now stop.\n");
+        return 0;
+    }
 
-    listen(sockfd, 10);
+    listen_loop(primary_listener);
 
     /* Exit */
     return 0;
@@ -47,6 +86,6 @@ int main(int argc, char *argv[])
 void print_usage()
 {
     printf("SailFish -> Ultra-Light, Ultra-Fast Web Server\n");
-    printf("Usage: SailFish port\n");
+    printf("Usage: sailfish port\n");
 }
 
