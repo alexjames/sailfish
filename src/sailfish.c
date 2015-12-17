3bdf7509eed73c0bad5738aa791fef3e993c6f25
alexjames <alx.james@gmail.com>
2015-12-18 00:44:26 +0530

    Code restructing. Correctly working 404 response.

--- src/sailfish.c
+++ src/sailfish.c
@@ -8,14 +8,45 @@
 #define PORT "8080"
 #define BACKLOG 10
 
+#define SF_CLNT_HTTP_VER_09 0
+#define SF_CLNT_HTTP_VER_10 1
+#define SF_CLNT_HTTP_VER_11 2
+
 #define SF_GET_MTHD 1
+#define SF_CLNT_ERR_400 9
+
+char *SF_404_RESP = "<html><title>404 Not Found</title><body><H1>NOT FOUND</H1><p>The requested URL could not be found.</p></body></html>";
 
 struct sailfish_http_rqst
 {
     int method;
+    int client_http_version;
     char uri[2048];
 };
 
+char* sf_get_response_phrase(int status)
+{
+    if (status == 404)
+    {
+        return "Not Found";
+    }
+}
+
+char* sf_get_client_http_version_str(struct sailfish_http_rqst *sf_str)
+{
+    if (sf_str->client_http_version == SF_CLNT_HTTP_VER_09)
+    {
+        return "HTTP/0.9";
+    }
+    else if (sf_str->client_http_version == SF_CLNT_HTTP_VER_10)
+    {
+        return "HTTP/1.0";
+    }
+    else
+    {
+        return "HTTP/1.1";
+    }
+}
 
 /* Starting message */
 void start_msg()
@@ -23,6 +54,7 @@ void start_msg()
     printf("=== Sailfish ===\n\n");
 }
 
+/* Parse HTML requests */ 
 int html_rqst_parser(char *rqst, struct sailfish_http_rqst *sf_str)
 {
     int rc;
@@ -30,6 +62,7 @@ int html_rqst_parser(char *rqst, struct sailfish_http_rqst *sf_str)
 
     if (sf_str == NULL)
     {
+        /* server error maybe?? */
         return -1;
     }
 
@@ -45,7 +78,14 @@ int html_rqst_parser(char *rqst, struct sailfish_http_rqst *sf_str)
 
     /* Check method */
     tok = strsep(&stringp, " ");
-    if (strncmp(tok, "GET", 3) == 0)
+    if (tok == NULL)
+    {
+        /* 400 Bad Request */
+        sf_str->method = SF_CLNT_ERR_400;
+        return 0;
+    }
+
+    if (strncmp(tok, "GET ", 4) == 0)
     {
         /* classic GET method */
         printf("GET method detected\n");
@@ -53,6 +93,12 @@ int html_rqst_parser(char *rqst, struct sailfish_http_rqst *sf_str)
         /* populate request structure */
         sf_str->method = SF_GET_MTHD;
         tok = strsep(&stringp, " ");
+        if (tok == NULL)
+        {
+            /* 400 Bad Request */
+            sf_str->method = SF_CLNT_ERR_400;
+            return 0;
+        }
         strncpy(sf_str->uri, tok, stringp - tok);
         printf("%d %s", sf_str->method, sf_str->uri);
     }
@@ -60,14 +106,56 @@ int html_rqst_parser(char *rqst, struct sailfish_http_rqst *sf_str)
     {
         /* looks like an unsupported method - 405 Method not allowed */
     }
+}
 
-    return 0;
+int generate_response(struct sailfish_http_rqst *sf_str, char *resp)
+{
+    int final_status;
+    char tmp[1000];
+    if (resp == NULL)
+    {
+        return -1;
+    }
+
+    if (sf_str == NULL)
+    {
+        return -1;   
+    }
+
+    /* Construct Status Line */
+    final_status = 404;
+    sf_str->client_http_version = SF_CLNT_HTTP_VER_11;
+    sprintf(tmp, "%s ", sf_get_client_http_version_str(sf_str)); 
+    strcat(resp, tmp);
+    sprintf(tmp, "%d ", final_status);
+    strcat(resp, tmp);
+    strcat(resp, sf_get_response_phrase(final_status));
+    strcat(resp, "\n"); 
+
+    /* Construct Entity Headers */
+    sprintf(tmp, "Content-Length: %d\n", strlen(SF_404_RESP));
+    strcat(resp, tmp);
+    strcat(resp, "Content-Type: text/html\n");
+    strcat(resp, "\n"); 
+
+    /* Append Data */
+    strcat(resp, SF_404_RESP);
+    printf("%s\n", resp);
 }
 
 
-void request_handler()
+void process_rqst(int conn, char* rqst)
 {
-
+    int rc;
+    char resp[1000] = {0};
+    struct sailfish_http_rqst st_rqst;
+    html_rqst_parser(rqst, &st_rqst);
+    generate_response(&st_rqst, resp);
+    rc = send(conn, resp, sizeof(resp), 0);
+    if (rc == -1)
+    {
+        fprintf(stderr, "send: %s\n", strerror(errno));
+    }
 }
 
 int main(int argc, char* argv[])
@@ -138,7 +226,6 @@ int main(int argc, char* argv[])
     {
         new_conn = accept(sockfd, &conn_info, &addrlen);
         char client_rqst[10024];
-        struct sailfish_http_rqst new_rqst;
         if (new_conn == -1)
         {
             fprintf(stderr, "accept: %s\n", strerror(errno));
@@ -152,7 +239,7 @@ int main(int argc, char* argv[])
             continue;
         }
         client_rqst[rc] = '\0';
-        html_rqst_parser(client_rqst, &new_rqst);
+        process_rqst(new_conn, client_rqst);
 
 /*
         rc = send(new_conn, "HTTP/1.0 200 OK\n"
